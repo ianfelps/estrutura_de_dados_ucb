f27ffe1e3cbe04ea74d0d43ecd690f6dfbffa7c2
ianfelps <iannfelps@gmail.com>
2024-04-02 11:19:52 -0300

    aula arquivos

---
+++ arquivos/create_arq.c
@@ -0,0 +1,35 @@
+/*
+Criar arquivos em C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    // declarar arquivo
+    FILE *arq_txt;
+
+    // abrir arquivo, apagar infos existentes e criar novas zeradas ("w")
+    // arq_txt = fopen("arq.txt", "w");
+
+    // abrir arquivo e adicionar infos ("a")
+    arq_txt = fopen("arq.txt", "a");
+
+    // verificar se o arquivo foi aberto
+    if (arq_txt == NULL){
+        printf("Erro ao abrir arquivo arq.txt");
+        exit(1);
+    }
+
+    // escrever no arquivo
+    fprintf(arq_txt, argv[1]);
+
+    // quebrar linha para nova mensagem
+    fprintf(arq_txt, "\n");
+
+    // fechar arquivo
+    fclose(arq_txt);
+
+    return 0;
+}
---
+++ arquivos/read_arq.c
@@ -0,0 +1,33 @@
+/*
+Ler arquivos em C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    char txt[100];
+    
+    // declarar arquivo
+    FILE *arq_txt;
+
+    // abrir arquivo para leitura ("r")
+    arq_txt = fopen("arq.txt", "r");
+
+    // verificar se o arquivo foi aberto
+    if (arq_txt == NULL){
+        printf("Erro ao abrir arquivo arq.txt");
+        exit(1);
+    }
+
+    // ler e printar strings do arquivo
+    while (fscanf(arq_txt, "%[^\n]\n", &txt) != EOF){
+        printf("%s ", txt);
+    }
+
+    // fechar arquivo
+    fclose(arq_txt);
+
+    return 0;
+}
