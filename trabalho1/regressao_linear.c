acdde949921ae23024413a9a96a0cf161c1d5a05
ianfelps <iannfelps@gmail.com>
2024-04-16 09:52:07 -0300

    start trabalho1

---
+++ trabalho1/regressao_linear.c
@@ -0,0 +1,17 @@
+/*
+Instruções
+No seu repositório de atividades no github, crie uma pasta chamada trabalho1, contendo um arquivo regressao_linear.c que implementa a solução do problema abaixo.
+
+Crie um programa em C que calcula a regressão linear a partir de um array de coordenadas (pontos).
+
+O programa deve receber por argumento o nome de um arquivo no formato ".csv", identificar a quantidade de registros no arquivo e alocar dinamicamente um array de struct Ponto. 
+
+A regressão linear é uma técnica de análise estatística que busca encontrar a linha que melhor se ajusta aos dados, minimizando os erros quadrados.
+
+Descrição
+- Leia os pontos (x,y) no arquivo informado;
+- Calcule a média dos valores de x e y;
+- Calcule a inclinação (coeficiente angular) da reta de regressão;
+- Calcule o ponto de intercepção (coeficiente linear) da reta de regressão;
+-Exiba os resultados da equação da regressão linear.
+*/
