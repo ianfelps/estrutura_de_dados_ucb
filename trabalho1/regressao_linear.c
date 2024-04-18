d05b50eec3527f2314be88f663ff97678c127d04
Ian Felipe <iannfelps@gmail.com>
2024-04-18 11:21:19 -0300

    update tb1

--- trabalho1/regressao_linear.c
+++ trabalho1/regressao_linear.c
@@ -15,3 +15,76 @@ Descrição
 - Calcule o ponto de intercepção (coeficiente linear) da reta de regressão;
 -Exiba os resultados da equação da regressão linear.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_REGISTROS 256 // definir numero maximo de registros
+
+typedef struct Ponto { // struct para armazenar os registros
+    int x;
+    float y;
+} Ponto;
+
+int main(int argc, char* argv[]){
+
+    // abrir arquivo com os dados a partir do argv
+    FILE *arq_dados = fopen(argv[1], "r");
+    if (arq_dados == NULL) {
+        printf("Erro ao abrir o arquivo %s\n", argv[1]);
+        return 1;
+    }
+
+    // verificar o número de registros no arquivo
+    int tamanho = 0;
+    char linhas[MAX_REGISTROS];
+    while (fgets(linhas, sizeof(linhas), arq_dados) != NULL) {
+        tamanho++;
+    }
+
+    // alocar struct na memoria
+    Ponto *pontos = (Ponto*) malloc(tamanho * sizeof(Ponto));
+    if (pontos == NULL) {
+        printf("Erro ao alocar registros na memória");
+        return 1;
+    }
+
+    // voltar ao inicio do arquive e ler os pontos
+    rewind(arq_dados);
+    float soma_x = 0, soma_y = 0; // declarar variaveis para calculo da media dos pontos
+    for (int i = 0; i < tamanho; i++) {
+        if (fgets(linhas, sizeof(linhas), arq_dados) == NULL) {
+            printf("Erro ao ler o arquivo");
+            return 1;
+        }
+        sscanf(linhas, "%d,%f", &(pontos[i].x), &(pontos[i].y));
+        soma_x += pontos[i].x;
+        soma_y += pontos[i].y;
+    }
+
+    // calcular a media dos pontos
+    float media_x = (float)soma_x / tamanho;
+    float media_y = soma_y / tamanho;
+
+    // calcular regressao linear
+    float div1 = 0, div2 = 0;
+    // inclinacao
+    for (int i = 0; i < tamanho; i++) { // somatorio de i = 1 ate n (tamanho)
+        div1 = (pontos[i].x - media_x) * (pontos[i].y - media_y); // (x'i - media_x) * (x'i - media_y)
+        div2 = (pontos[i].x - media_x) * (pontos[i].x - media_x); // (x'i - media_x)^2
+    }
+    float inclinacao = div1 / div2;
+    // intercepcao
+    float intercepcao = media_y - inclinacao * media_x; // media_y - inclinacao * media_x
+
+    // printar o resultado
+    printf("Resultado: y = %.2fx + %.2f\n", inclinacao, intercepcao); // y = inclinacao * x + intercepcao
+
+    // liberar memoria
+    free(pontos);
+
+    // fechar o arquivo
+    fclose(arq_dados);
+
+    return 0;
+}
