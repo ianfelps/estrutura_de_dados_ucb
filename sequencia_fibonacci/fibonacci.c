c259eb0e16707478b4c98321746cdcdddcb72a55
ianfelps <iannfelps@gmail.com>
2024-08-21 10:05:29 -0300

    update fibonacci

--- sequencia_fibonacci/fibonacci.c
+++ sequencia_fibonacci/fibonacci.c
@@ -1,21 +1,33 @@
+// algoritmo para calcular a n-esima posicao na sequencia de fibonacci
+
 #include <stdio.h>
 #include <stdlib.h>
+
+// funcao recursiva
+long fibo(int posicao){
+    if (posicao <= 2){
+        return 1;
+    } else {
+        return fibo(posicao - 1) + fibo(posicao - 2);
+    }
+}
 
 int main(){
 
     int pos;
-    long num1 = 1, num2 = 1, aux;
+    // long num1 = 1, num2 = 1, aux;
 
     printf("Digite a posicao do termo de Fibonacci: ");
     scanf("%d", &pos);
 
+    /* calculo com for
     for (int i = 2; i < pos; i++){
         aux = num2;
         num2 = num1 + num2;
         num1 = aux;
-    }
+    } */
 
-    printf("O %do da sequencia de Fibonacci eh %ld!", pos, num2);
+    printf("O %do da sequencia de Fibonacci eh %ld!", pos, fibo(pos));
 
     return 0;
 }
